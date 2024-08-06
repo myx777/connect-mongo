39430d58f2392beed495da5118299299e9d85a2c
myx777 <anton.kruglov89@gmail.com>
2024-08-06 15:43:40 +0300

    add @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {MongooseModule} from '@nestjs/mongoose';
+import * as process from "node:process";
 
 @Module({
     imports: [
         // подключаем монго
-        MongooseModule.forRoot('mongodb://example:example@localhost:27017/example'),
+        MongooseModule.forRoot(process.env.MONGO_CONNECTION),
     ],
     controllers: [AppController],
     providers: [AppService],
